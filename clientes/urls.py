0f3609b5f065fdb86f4ce06a9b299d5c40494d19
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2023-06-28 22:12:25 -0300

    clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-     path(''. )
+     path('', views.clientes, name="clientes" )
      
 ]
--- siga_transportes/settings.py
+++ siga_transportes/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clientes'
 ]
 
 MIDDLEWARE = [
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'siga_transportes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
